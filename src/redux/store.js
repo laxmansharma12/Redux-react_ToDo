303c524093d73bb2d5cb390be5e04fe8ac99b61f
laxman <laxmansharmawe@gmail.com>
2024-03-08 00:01:39 +0530

    removed unwanted dependencies

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,14 @@
 import { createStore } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // localStorage
-
 import todoReducer from "./reducer";
 
 const persistConfig = {
-  key: "root",
-  storage,
+	key: "root",
+	storage,
 };
 
 const persistedReducer = persistReducer(persistConfig, todoReducer);
-
 const store = createStore(persistedReducer);
 const persistor = persistStore(store);
 
