0a6dc503d85ee2e2faa62ddcb15581464ad5054b
laxman <laxmansharmawe@gmail.com>
2024-03-07 23:45:18 +0530

    added add word

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,41 +5,41 @@ import FilterButtons from "./FilterButtons";
 import { addTodo } from "../redux/actions";
 
 const Todo = () => {
-  const dispatch = useDispatch();
-  const [newTodoText, setNewTodoText] = useState("");
+	const dispatch = useDispatch();
+	const [newTodoText, setNewTodoText] = useState("");
 
-  const handleAddTodo = (text) => {
-    dispatch(addTodo(text));
-  };
+	const handleAddTodo = (text) => {
+		dispatch(addTodo(text));
+	};
 
-  const handleAddTodoClick = () => {
-    if (newTodoText.trim() !== "") {
-      handleAddTodo(newTodoText.trim());
-      setNewTodoText("");
-    }
-  };
+	const handleAddTodoClick = () => {
+		if (newTodoText.trim() !== "") {
+			handleAddTodo(newTodoText.trim());
+			setNewTodoText("");
+		}
+	};
 
-  return (
-    <div>
-      <h4>ToDo </h4>
-      <div>
-        <input
-          type="text"
-          id="addTodoInput"
-          placeholder="Type your Todo"
-          value={newTodoText}
-          onChange={(e) => setNewTodoText(e.target.value)}
-        />
-        <button onClick={handleAddTodoClick}> + </button>
-      </div>
+	return (
+		<div>
+			<h4>ToDo </h4>
+			<div>
+				<input
+					type="text"
+					id="addTodoInput"
+					placeholder="Type your Todo"
+					value={newTodoText}
+					onChange={(e) => setNewTodoText(e.target.value)}
+				/>
+				<button onClick={handleAddTodoClick}>ADD </button>
+			</div>
 
-      <div>
-        <FilterButtons />
-      </div>
+			<div>
+				<FilterButtons />
+			</div>
 
-      <TodoList />
-    </div>
-  );
+			<TodoList />
+		</div>
+	);
 };
 
 export default Todo;
